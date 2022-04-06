990e4bb15884735c7594575189b4e07345f7402a
Marina LaBonte <88249568+Litsavkina@users.noreply.github.com>
2022-04-06 15:22:24 -0400

    MakeEnds

--- src/MakeEnds.java
+++ src/MakeEnds.java
@@ -7,4 +7,9 @@ makeEnds([1, 2, 3]) → [1, 3]
 makeEnds([1, 2, 3, 4]) → [1, 4]
 makeEnds([7, 4, 6, 2]) → [7, 2]
      */
+
+    public static int[] makeEnds(int[] nums) {
+        return new int[]{nums[0], nums[nums.length - 1]};
+    }
+
 }
